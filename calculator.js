056c9bcbe347bcd7ccd36736a02be93d9cab8a87
harut0011 <metsatunyanharut7@gmail.com>
2020-01-23 19:57:51 +0300

    day1

--- calculator.js
+++ calculator.js
@@ -1,21 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let buttons = document.querySelectorAll('button');
-	let currentNum = document.querySelector('.current-num');
-	let span = document.querySelector('span')
+    'use strict'
 
-	for (let a = 0; a < buttons.length; a++) {
-		let button = buttons[a];
-		button.addEventListener('click', () => {
-			
-			if (button.innerHTML > 0) {
-				if (currentNum.innerHTML == '0') {
-					currentNum.innerHTML = button.innerHTML;
+    const currentNum = document.querySelector('.current-num');
+    const span = document.querySelector('span'); // the showing text when there's any click on buttons
+    const refresh = document.getElementById('refresh'); // button C to refress the current number
+    const numbers = document.querySelectorAll('.number'); // all button numbers
+    const maths = document.querySelectorAll('.math'); // arithmetic operation buttons
+
+    let spanEff = (spanValue) => {
+        span.innerHTML = spanValue;
+        span.classList.add('span-eff');
+        span.style.opacity = '1';
+        setTimeout(() => {
+
+            span.style.opacity = '0';
+
+            setTimeout(() => {
+                span.classList.remove('span-eff');
+            }, 200)
+        }, 200)
+    }
+
+    for (let a = 0; a < numbers.length; a++) {
+        let number = numbers[a];
+        number.addEventListener('click', () => { //number buttons sorting out
+            if (currentNum.innerHTML == '0') {
+
+                currentNum.innerHTML = number.innerHTML;
 
 
-				} else {
-					currentNum.innerHTML += button.innerHTML;
-				}
-			}
-		})
-	}
+                span.innerHTML = number.innerHTML
+
+            } else {
+                currentNum.innerHTML += number.innerHTML;
+
+                spanEff(number.innerHTML)
+            }
+        });
+
+
+    }
+
+    for (let b = 0; b < maths.length; b++) { //math buttons sorting out
+        let math = maths[b];
+
+        math.addEventListener('click', () => {
+            if (math.innerHTML == '+') {
+                spanEff('plus')
+            } else if (math.innerHTML == '-') {
+                spanEff('minus')
+            } else if (math.innerHTML == '*') {
+                spanEff('times')
+            } else if (math.innerHTML == '/') {
+                spanEff('divided by')
+            } else if (math.innerHTML == '.') {
+                spanEff('.')
+            }
+        })
+    }
+
+    refresh.addEventListener('click', () => {
+        currentNum.innerHTML = '0'
+    })
 })
